ed373d37e821f4a551c7a3be8692176972d9aa76
Somayyeh094 <s.somayyeh.seyedi@gmail.com>
2025-12-28 23:29:53 +0330

    fix a bug in js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//variables
 let randomNumber = Math.trunc(Math.random() * 20) + 1;
 const secretNumber = document.querySelector(".secret-number");
 const scoringMessage = document.querySelector(".scoring__message");
@@ -5,36 +6,50 @@ const scoreHTML = document.querySelector(".scoring__score");
 const highScoreHTML = document.querySelector(".scoring__highScore");
 const checkBtn = document.querySelector(".check");
 const againBtn = document.querySelector(".again");
-const win = new Audio("audio/win.mp3").preload;
-const fail = new Audio("audio/fail.mp3").preload;
+// const win = new Audio("audio/win.mp3").preload;
+// const fail = new Audio("audio/fail.mp3").preload;
 let score = 15;
 let highScore = 0;
 
+//check button event
 checkBtn.addEventListener("click", function (e) {
   e.preventDefault();
-  const numberInput = +document.querySelector(".number-input").value;
-  if (numberInput < 1 || numberInput > 20) {
-    alert("Please enter a number between 1-20!");
-  } else if (numberInput === randomNumber) {
-    win.play();
-    document.querySelector("body").style.backgroundColor = "#60b347";
-    secretNumber.style.width = "30rem";
-    secretNumber.textContent = numberInput;
-    scoringMessage.textContent = "You won the game!🥳🥳🥳";
-    if (score > highScore) highScoreHTML.textContent = score;
+  if (score === 0) {
+    alert("Start a new game! 😏");
   } else {
-    scoringMessage.textContent =
-      numberInput > randomNumber ? "⬆️ Too high" : "⬇️ Too low";
-    if (score > 1) {
-      score--;
-      scoreHTML.textContent = score;
+    const numberInput = +document.querySelector(".number-input").value;
+    if (numberInput < 1 || numberInput > 20) {
+      //check if the number is in range
+      alert("Please enter a number between 1-20! 🫤");
+    } else if (numberInput === randomNumber) {
+      //check if the number equals to the random number
+      new Audio("audio/win.mp3").play();
+      document.querySelector("body").style.backgroundColor = "#60b347";
+      secretNumber.style.width = "30rem";
+      secretNumber.textContent = numberInput;
+      scoringMessage.textContent = "You won the game!🥳🥳🥳";
+      if (score > highScore) {
+        highScore = score;
+        highScoreHTML.textContent = highScore;
+      }
     } else {
-      fail.play();
-      scoringMessage.textContent = "You lost the game!😎😎😎";
+      //when the number is different from the random number
+      scoringMessage.textContent =
+        numberInput > randomNumber ? "⬆️ Too high" : "⬇️ Too low";
+      if (score > 1) {
+        score--;
+        scoreHTML.textContent = score;
+      } else {
+        new Audio("audio/fail.mp3").play();
+        score = 0;
+        scoreHTML.textContent = score;
+        scoringMessage.textContent = "You lost the game!😎😎😎";
+      }
     }
   }
 });
 
+//again button event
 againBtn.addEventListener("click", function () {
   document.querySelector("body").style.backgroundColor = "#222";
   secretNumber.textContent = "?";
